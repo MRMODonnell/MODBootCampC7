da0d664a7aa95b10ed8d8c5562da6a72dd4b3110
MRMODonnell <mrmodonnell@hotmail.com>
2023-01-20 15:25:57 +0000

    created function to set time to refresh every second

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,5 +3,11 @@ let timeString = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5P
 let currentHour = moment().format('h:mm:ss a');
 let currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 
-
-
+// function to set the current date & Time - and append to 
+let interval = setInterval(function() {
+    let momentNow = moment();
+    $('#currentDay').append(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
