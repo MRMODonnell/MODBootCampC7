a79302b5751d39ef4271cca63ab59416cf0e6cc6
MRMODonnell <mrmodonnell@hotmail.com>
2023-01-20 15:11:31 +0000

    added two time blocks to see if they sho9w up on page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,7 @@
 let container = $(".container");
 let timeString = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 let currentHour = moment().format('h:mm:ss a');
+let currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 
-var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+
+
